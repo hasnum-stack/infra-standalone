fef5fabf8018b40bdf50c461fbc3d3c4ab00687d
hasnum <hasnum@163.com>
2024-12-31 09:57:08 +0800

    ✨(zustand): add type

--- src/galaxies/TodoList/store/index.ts
+++ src/galaxies/TodoList/store/index.ts
@@ -1,6 +1,12 @@
 import { create } from 'zustand';
 
-const useStore = create((set) => ({
+interface Todo {
+  todos: string[];
+  addTodo: (todo: string) => void;
+  removeTodo: (todo: string) => void;
+  clearTodos: () => void;
+}
+const useStore = create<Todo>()((set) => ({
   todos: [],
   addTodo: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
   removeTodo: (todo) =>
